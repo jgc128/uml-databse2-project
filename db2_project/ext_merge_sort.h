f815f1fa11d0f771e6bd845dd7d1fdaaf29f07a2
Alexey Romanov <jgc128@outlook.com>
2016-03-08 21:03:12 -0500

    create runs , write runs to disk

--- db2_project/ext_merge_sort.h
+++ db2_project/ext_merge_sort.h
@@ -11,32 +11,35 @@ using namespace std;
 
 #define PAGE_SIZE (4 * 1024) // page size is 4K
 
+struct Run {
+	unsigned long pos;
+	size_t records;
+};
+
 // T - the type of the records
 // RS - the size of the records in the file
 template<typename T, unsigned long RS> 
 class ExternalMergeSort{
 protected:
-
-	unsigned long records_in_page();
-	unsigned long pages_in_memory();
-
-
 	ifstream input;
 	fstream output;
-
-	// https://en.wikipedia.org/wiki/Seekg
-	//fstream myFile("test.txt", ios::in | ios::out | ios::trunc);
 
 	unsigned long order;
 	unsigned long memory_limit;
 
-	QuickSort<long long> sorting;
+	const char fill_char;
+
+	unsigned long records_in_page();
+	unsigned long pages_in_memory();
 
 	vector<T> read_records();
 	vector<T> read_records(unsigned long n);
 	vector<T> read_records(unsigned long n, unsigned long pos);
 
-	vector<size_t> generate_runs();
+	void write_records(vector<T> records);
+	void write_records(vector<T> records, unsigned long pos);
+
+	vector<Run> create_runs();
 
 
 public:
@@ -49,12 +52,14 @@ public:
 
 
 template<typename T, unsigned long RS> ExternalMergeSort<T, RS>::ExternalMergeSort(string input_filename, string output_filename, unsigned long order, unsigned long memory_limit)
+	: fill_char('0')
 {
 	this->order = order;
 	this->memory_limit = memory_limit;
 
 
 	input.open(input_filename);
+	output.open(output_filename, ios::in | ios::out | ios::trunc);
 }
 template<typename T, unsigned long RS> ExternalMergeSort<T, RS>::~ExternalMergeSort()
 {
@@ -74,24 +79,36 @@ template<typename T, unsigned long RS> unsigned long ExternalMergeSort<T, RS>::p
 
 template<typename T, unsigned long RS> void ExternalMergeSort<T, RS>::sort()
 {
-	auto runs_boundaries = generate_runs();
+	auto runs_boundaries = create_runs();
 }
 
-template<typename T, unsigned long RS> vector<size_t> ExternalMergeSort<T, RS>::generate_runs()
+template<typename T, unsigned long RS> vector<Run> ExternalMergeSort<T, RS>::create_runs()
 {
-	auto nb_records_to_read = records_in_page() * pages_in_memory();
-	
+	auto records_to_read = 4;// records_in_page() * pages_in_memory();
 
-	auto records1 = read_records(4);
-	for (auto &r : records1)
+	QuickSort<T> quick_sort;
+	vector<Run> runs;
+
+	unsigned long cur_pos = 0;
+	unsigned long cur_records = 0;
+	do
 	{
-		cout << r << endl;
-	}
-	cout << endl;
+		auto records = read_records(records_to_read);
 
+		quick_sort.sort(records);
+		write_records(records);
 
+		cur_records = records.size();
+		Run r = {
+			cur_pos, 
+			cur_records
+		};
+		runs.push_back(r);
 
-	return vector<size_t>(10);
+		cur_pos += cur_records;
+	} while (cur_records == records_to_read);
+
+	return runs;
 }
 
 template<typename T, unsigned long RS> vector<T> ExternalMergeSort<T, RS>::read_records()
@@ -140,3 +157,21 @@ template<typename T, unsigned long RS> vector<T> ExternalMergeSort<T, RS>::read_
 
 	return read_records(n);
 }
+
+template<typename T, unsigned long RS> void ExternalMergeSort<T, RS>::write_records(vector<T> records)
+{
+	for (T &r : records)
+	{
+		output << right << setfill(fill_char) << setw(RS) << r << endl;
+	}
+}
+
+template<typename T, unsigned long RS> void ExternalMergeSort<T, RS>::write_records(vector<T> records, unsigned long pos)
+{
+	auto real_pos = pos * (RS + 1); // +1 for the \n
+	output.seekp(real_pos);
+
+	write_records(records);
+}
+
+
