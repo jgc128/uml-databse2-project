02423a2f3b9ba9557046d2a099eb339ac1005f8f
Alexey Romanov <jgc128@outlook.com>
2016-03-09 19:15:53 -0500

    initial merge

--- db2_project/ext_merge_sort.h
+++ db2_project/ext_merge_sort.h
@@ -3,6 +3,8 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <queue>
+#include <functional>
 
 #include "utils.h"
 #include "quick_sort.h"
@@ -17,6 +19,24 @@ struct Run {
 	size_t records;
 };
 
+template<typename T> struct RecordRunInfo {
+	T value;
+	unsigned long run;
+};
+template<typename T> inline bool operator < (const RecordRunInfo<T> & left, const RecordRunInfo<T> & right)
+{
+	return left.value < right.value;
+}
+template<typename T> inline bool operator > (const RecordRunInfo<T> & left, const RecordRunInfo<T> & right)
+{
+	return operator < (right, left);
+}
+template<typename T> inline bool operator == (const RecordRunInfo<T> & left, const RecordRunInfo<T> & right)
+{
+	return left.value == right.value;
+}
+
+
 // T - the type of the records
 // RS - the size of the records in the file
 template<typename T, unsigned long RS> 
@@ -24,6 +44,7 @@ class ExternalMergeSort{
 protected:
 	RecordIO<T, RS> input;
 	RecordIO<T, RS> output;
+	RecordIO<T, RS> tmp_file;
 
 	unsigned long order;
 	unsigned long memory_limit;
@@ -31,11 +52,8 @@ protected:
 	unsigned long records_in_page();
 	unsigned long pages_in_memory();
 
-	void print_runs(const vector<Run> & runs);
-
-	vector<Run> create_runs();
-
-
+	queue<Run> create_runs();
+	void merge(queue<Run> & runs, unsigned long n);
 
 public:
 	ExternalMergeSort(string input_filename, string output_filename, unsigned long order, unsigned long memory_limit);
@@ -47,7 +65,9 @@ public:
 
 
 template<typename T, unsigned long RS> ExternalMergeSort<T, RS>::ExternalMergeSort(string input_filename, string output_filename, unsigned long order, unsigned long memory_limit)
-	: input(input_filename, ios::in), output(output_filename, ios::in | ios::out | ios::trunc)
+	: input(input_filename, ios::in)
+	, output(output_filename, ios::in | ios::out | ios::trunc)
+	, tmp_file(output_filename + "_tmp", ios::in | ios::out | ios::trunc)
 {
 	this->order = order;
 	this->memory_limit = memory_limit;
@@ -59,44 +79,79 @@ template<typename T, unsigned long RS> ExternalMergeSort<T, RS>::~ExternalMergeS
 
 template<typename T, unsigned long RS> unsigned long ExternalMergeSort<T, RS>::records_in_page()
 {
-	return PAGE_SIZE / RS;
+	return 3; //PAGE_SIZE / RS;
 }
 template<typename T, unsigned long RS> unsigned long ExternalMergeSort<T, RS>::pages_in_memory()
 {
-	return (memory_limit * 1024 * 1024) / PAGE_SIZE;
-}
-
-template<typename T, unsigned long RS> void ExternalMergeSort<T, RS>::print_runs(const vector<Run> & runs)
-{
-	cout << padded_string("Runs:") << runs.size() << endl;
-	for (auto &r : runs)
-	{
-		cout << padded_string("run:") << r.pos << ":" << r.records << endl;
-	}
+	return 1; //(memory_limit * 1024 * 1024) / PAGE_SIZE;
 }
 
 
 template<typename T, unsigned long RS> void ExternalMergeSort<T, RS>::sort()
 {
 	auto runs = create_runs();
-	print_runs(runs);
+	cout << padded_string("Runs:") << runs.size() << endl;
 
-
-
-
-
-
+	merge(runs, 10);
 
 
 	int i = 0;
 }
 
-template<typename T, unsigned long RS> vector<Run> ExternalMergeSort<T, RS>::create_runs()
+template<typename T, unsigned long RS> void ExternalMergeSort<T, RS>::merge(queue<Run> & runs, unsigned long n)
 {
-	auto records_to_read = 4; // records_in_page() * pages_in_memory();
+	vector<Run> runs_to_merge(n);
+	for (unsigned i = 0;i < n; i++)
+	{
+		auto r = runs.front();
+		runs_to_merge[i] = r;
+		runs.pop();
+	}
+
+	priority_queue<RecordRunInfo<T>, vector<RecordRunInfo<T>>, greater<RecordRunInfo<T>>> records;
+	
+	// set the initial positions of the runs
+	vector<unsigned long> runs_postions(n);
+	for (unsigned i = 0; i < n; i++)
+	{
+		runs_postions[i] = runs_to_merge[i].pos;
+	}
+
+	// fill the priority queue initially
+	for (unsigned i = 0; i < n; i++)
+	{
+		auto rec = output.read_records(1, runs_postions[i]);
+		RecordRunInfo<T> rec_run_info = { rec[0], i };
+		records.push(rec_run_info);
+
+		runs_postions[i] += 1;
+	}
+
+	while(records.size() != 0)
+	{
+		auto smallest_rec = records.top();
+		records.pop();
+
+		tmp_file.write_record(smallest_rec.value);
+
+		if (runs_postions[smallest_rec.run] < runs_to_merge[smallest_rec.run].pos + runs_to_merge[smallest_rec.run].records)
+		{
+			auto rec = output.read_records(1, runs_postions[smallest_rec.run]);
+			RecordRunInfo<T> rec_run_info = { rec[0], smallest_rec.run };
+			records.push(rec_run_info);
+
+			runs_postions[smallest_rec.run] += 1;
+		}
+	}
+
+}
+
+template<typename T, unsigned long RS> queue<Run> ExternalMergeSort<T, RS>::create_runs()
+{
+	auto records_to_read = records_in_page() * pages_in_memory();
 
 	QuickSort<T> quick_sort;
-	vector<Run> runs;
+	queue<Run> runs;
 
 	unsigned long cur_pos = 0;
 	unsigned long cur_records = 0;
@@ -110,7 +165,9 @@ template<typename T, unsigned long RS> vector<Run> ExternalMergeSort<T, RS>::cre
 			output.write_records(records);
 
 			Run r = { cur_pos, cur_records };
-			runs.push_back(r);
+			runs.push(r);
+
+			cout << padded_string("run:") << r.pos << ":" << r.records << endl;
 		}
 
 		cur_pos += cur_records;
--- db2_project/record_io.h
+++ db2_project/record_io.h
@@ -22,9 +22,19 @@ public:
 	vector<T> read_records();
 	vector<T> read_records(unsigned long n);
 	vector<T> read_records(unsigned long n, unsigned long pos);
+	//T read_record();
+	//T read_record(unsigned long pos);
 
 	void write_records(vector<T> records);
 	void write_records(vector<T> records, unsigned long pos);
+	void write_record(T record);
+	void write_record(T record, unsigned long pos);
+
+	inline unsigned long get_file_position(unsigned long pos);
+	void set_read_postion(unsigned long pos);
+	void set_write_postion(unsigned long pos);
+
+	void flush();
 };
 
 
@@ -36,13 +46,32 @@ template<typename T, unsigned long RS> RecordIO<T, RS>::RecordIO(string filename
 template<typename T, unsigned long RS> RecordIO<T, RS>::RecordIO(string filename, ios_base::openmode mode)
 	: fill_char('0')
 {
-	file.open(filename, mode);
+	file.open(filename, mode | ios::binary);
 }
 template<typename T, unsigned long RS> RecordIO<T, RS>::~RecordIO()
 {
 	file.close();
 }
 
+template<typename T, unsigned long RS> inline unsigned long RecordIO<T, RS>::get_file_position(unsigned long pos)
+{
+	return pos * (RS + 1); // +1 for the \n
+}
+template<typename T, unsigned long RS> void RecordIO<T, RS>::set_read_postion(unsigned long pos)
+{
+	auto real_pos = get_file_position(pos);
+	file.seekg(real_pos);
+}
+template<typename T, unsigned long RS> void RecordIO<T, RS>::set_write_postion(unsigned long pos)
+{
+	auto real_pos = get_file_position(pos);
+	file.seekp(real_pos);
+}
+
+template<typename T, unsigned long RS> void RecordIO<T, RS>::flush()
+{
+	file.flush();
+}
 
 template<typename T, unsigned long RS> vector<T> RecordIO<T, RS>::read_records()
 {
@@ -85,15 +114,24 @@ template<typename T, unsigned long RS> vector<T> RecordIO<T, RS>::read_records(u
 
 template<typename T, unsigned long RS> vector<T> RecordIO<T, RS>::read_records(unsigned long n, unsigned long pos)
 {
-	auto real_pos = pos * (RS + 1); // +1 for the \n
-	file.seekg(real_pos);
-
+	set_read_postion(pos);
 	return read_records(n);
 }
 
+//template<typename T, unsigned long RS> T RecordIO<T, RS>::read_record()
+//{
+//
+//}
+//template<typename T, unsigned long RS> T RecordIO<T, RS>::read_record(unsigned long pos)
+//{
+//	set_read_postion(pos);
+//	return read_record();
+//}
+
+
 template<typename T, unsigned long RS> void RecordIO<T, RS>::write_records(vector<T> records)
 {
-	for (T &r : records)
+	for (auto const &r : records)
 	{
 		file << right << setfill(fill_char) << setw(RS) << r << endl;
 	}
@@ -101,8 +139,16 @@ template<typename T, unsigned long RS> void RecordIO<T, RS>::write_records(vecto
 
 template<typename T, unsigned long RS> void RecordIO<T, RS>::write_records(vector<T> records, unsigned long pos)
 {
-	auto real_pos = pos * (RS + 1); // +1 for the \n
-	file.seekp(real_pos);
-
+	set_write_postion(pos);
 	write_records(records);
+}
+
+template<typename T, unsigned long RS> void RecordIO<T, RS>::write_record(T record)
+{
+	file << right << setfill(fill_char) << setw(RS) << record << endl;
+}
+template<typename T, unsigned long RS> void RecordIO<T, RS>::write_record(T record, unsigned long pos)
+{
+	set_write_postion(pos);
+	write_record(record);
 }
