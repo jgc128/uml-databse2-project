4eaab46ed60e0601ed71d271777deeffbf0eb409
Alexey Romanov <jgc128@outlook.com>
2016-03-10 21:27:19 -0500

    Using RecordIO in quicksort

--- db2_project/utils.cpp
+++ db2_project/utils.cpp
@@ -7,33 +7,3 @@
 
 using namespace std;
 
-/* =======================================
-=== File IO Utilities ====================
-========================================*/
-
-vector<long long> load_file(string filename)
-{
-	ifstream file(filename);
-	vector<long long> results;
-
-	if (file.is_open())
-	{
-		long long number;
-		string line;
-
-		while (getline(file, line))
-		{
-			number = stoll(line);
-			results.push_back(number);
-		}
-
-		file.close();
-	}
-	else
-	{
-		cerr << "Unable to open file " << filename << endl;
-	}
-
-	return results;
-}
-
